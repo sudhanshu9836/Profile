d52ed0710d9437801ad599fd4b412e4e027325a9
Sudhanshu Tiwari <sudhanshutiwari9836@gmail.com>
2024-08-27 00:39:37 +0530

    Added signup page

---
+++ Frontend/components/signupPage.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './signupPage.css'
+
+function SignupPage() {
+  return (
+    <>
+    <div className="main-box">
+    <img src="https://i.pinimg.com/564x/be/59/bd/be59bdee35f2aec94d9e90a30ee5233d.jpg" alt="image" id='signupImage' />
+    <div className='right'>
+        <div className="r1">
+        <h2>SignUp</h2>
+        <p>Join us today !</p>
+        </div>
+        <div className="r2">
+            <div className="field">
+                <label htmlFor="email">Email</label>
+                <input type="email" id='email' name='email' placeholder='Enter your email'/>
+            </div>
+            <div className="field">
+                <label htmlFor="contact">Mobile no.</label>
+                <input type="number" id='contact' name='contact' placeholder='Enter your mobile number'/>
+            </div>
+            <div className="field">
+                <label htmlFor="password">Password</label>
+                <input type="password" id='password' name='password' placeholder='Enter your password'/>
+            </div>
+            <div className="field">
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <input type="password" id='password' name='confirmPassword' placeholder='Enter your password again'/>
+            </div>  
+        </div>
+        <div className="r3">
+        <button>Sign Up</button>
+        <p>Have an acoount ? <a href="">Sign in</a></p>
+        </div>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default SignupPage
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import LoginPage from "../components/loginPage"
+// import LoginPage from "../components/loginPage"
+import SignupPage from "../components/signupPage"
 
 
 function App() {
   return (
     <>
-     <LoginPage/>
+     <SignupPage/>
     </>
   )
 }
